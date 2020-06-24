cb725b7a88173c771927e333ff07b0cf341afd44
Kirill Gubich <kirillgubich@ya.ru>
2020-06-24 19:02:03 +0300

    commit

--- Basics/Cycles/Task4.java
+++
@@ -1,9 +0,0 @@
-// Составить программу нахождения произведения квадратов первых двухсот чисел
-package by.jonline.cycle.main;
-
-public class Task4 {
-	
-	public static void main(String[] args) {
-		int num;
-	}
-}
