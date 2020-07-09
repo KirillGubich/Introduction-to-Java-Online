f24cc7e7db6fa194ca902b6e8586b92c2ef30de6
Kirill Gubich <kirillgubich@ya.ru>
2020-07-09 15:03:46 +0300

    just commit

--- Basics/Branches/Task1.java
+++ Basics/Branches/Task1.java
@@ -8,9 +8,9 @@ public class Task1 {
 	
     public static void main(String[] args) {
 		
-		// Ввод данных
-	    Scanner firstInp = new Scanner(System.in);
-	    Scanner secondInp = new Scanner(System.in);
+        // Ввод данных
+        Scanner firstInp = new Scanner(System.in);
+        Scanner secondInp = new Scanner(System.in);
         int firstAngle = firstInp.nextInt();
          int secondAngle = secondInp.nextInt();
 
